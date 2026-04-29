a6dd59e6db21646569d2808790d87c19a129296d
atanasbondzhakov <atanas.b@yahoo.com>
2026-04-29 09:36:13 +0300

    Install React Navigation and setup it for first screen

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import CategoriesScreen from './screens/CategoriesScreen.js';
 import { StatusBar } from 'expo-status-bar';
 
+const Stack = createStackNavigator();
+
 export default function App() {
     return (
         <>
-            <StatusBar style='light' />
-            <CategoriesScreen />
+            <StatusBar style='dark' />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name='MealCategories' component={CategoriesScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </>
     );
 }
